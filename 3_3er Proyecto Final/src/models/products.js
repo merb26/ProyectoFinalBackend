8edab0107e7306e2e9032e67d5e9a9034355c525
Manuel Esteban (Merb) <manuele.ramirez.26@gmail.com>
2022-12-21 08:26:33 -0600

    quito urlPhoto del modelo users

--- 3_3er Proyecto Final/src/models/products.js
+++ 3_3er Proyecto Final/src/models/products.js
@@ -1,15 +1,15 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose';
 
-const productsCollection = "products"
+const productsCollection = 'products';
 
 const productSchema = new mongoose.Schema({
-  name: { type: String, require: true, max: 100 },
-  description: { type: String },
-  code: { type: String, require: true },
-  urlPicture: { type: String, require: true },
-  price: { type: String, require: true },
-  stock: { type: String, require: true },
-  timestamp: { tyep: String },
-})
+  name: {type: String, require: true, max: 100},
+  description: {type: String},
+  code: {type: String, require: true},
+  urlPicture: {type: String, require: true},
+  price: {type: String, require: true},
+  stock: {type: String, require: true},
+  timestamp: {tyep: String},
+});
 
-export default mongoose.model(productsCollection, productSchema)
+export default mongoose.model(productsCollection, productSchema);
--- 3_3er Proyecto Final/src/models/users.js
+++ 3_3er Proyecto Final/src/models/users.js
@@ -1,14 +1,13 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose';
 
-const nameCollection = "users"
+const nameCollection = 'users';
 
 const schemaUser = new mongoose.Schema({
-  email: { type: String, require: true },
-  password: { type: String, require: true },
-  name: { type: String, require: true },
-  address: { type: String, require: true },
-  phone: { type: String, require: true },
-  urlPhoto: { type: String, require: true },
-})
+  email: {type: String, require: true},
+  password: {type: String, require: true},
+  name: {type: String, require: true},
+  address: {type: String, require: true},
+  phone: {type: String, require: true},
+});
 
-export default mongoose.model(nameCollection, schemaUser)
+export default mongoose.model(nameCollection, schemaUser);
