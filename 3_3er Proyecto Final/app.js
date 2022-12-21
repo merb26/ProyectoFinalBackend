75e87305c668ea57d97e0ef9053e54ee4b544515
Manuel Esteban (Merb) <manuele.ramirez.26@gmail.com>
2022-12-21 08:19:10 -0600

    elimino el funcionamiento de subir la foto

--- 3_3er Proyecto Final/app.js
+++ 3_3er Proyecto Final/app.js
@@ -1,44 +1,43 @@
-import yargs from "yargs/yargs"
-const args = yargs(process.argv.slice(2)).argv
-import express from "express"
-import fileUpload from "express-fileupload"
-import session from "express-session"
-const app = express()
-import cookieParser from "cookie-parser"
-import MongoStore from "connect-mongo"
-import passport from "passport"
-import mongoose from "mongoose"
-import cluster from "cluster"
-import numCPUs from "os"
-const cpus = numCPUs.cpus().length
-import * as dotenv from "dotenv"
-dotenv.config()
+import yargs from 'yargs/yargs';
+const args = yargs(process.argv.slice(2)).argv;
+import express from 'express';
+import fileUpload from 'express-fileupload';
+import session from 'express-session';
+const app = express();
+import cookieParser from 'cookie-parser';
+import MongoStore from 'connect-mongo';
+import passport from 'passport';
+import mongoose from 'mongoose';
+import cluster from 'cluster';
+import numCPUs from 'os';
+const cpus = numCPUs.cpus().length;
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-import { routeLogin } from "./src/routes/routeLogin.js"
-import { routeRegister } from "./src/routes/routeRegister.js"
-import { routeProducts } from "./src/routes/routeProducts.js"
-import { routerCar } from "./src/routes/routerCar.js"
-import { routeOrder } from "./src/routes/routeOrder.js"
-import { runServer } from "./server.js"
-import { loggerCons, loggerWarn } from "./loggers/logger.js"
+import {routeLogin} from './src/routes/routeLogin.js';
+import {routeRegister} from './src/routes/routeRegister.js';
+import {routeProducts} from './src/routes/routeProducts.js';
+import {routerCar} from './src/routes/routerCar.js';
+import {routeOrder} from './src/routes/routeOrder.js';
+import {loggerCons, loggerWarn} from './loggers/logger.js';
 
-app.use(express.static("public"))
-app.use(express.json())
-app.use(cookieParser())
-app.use(express.urlencoded({ extended: true }))
-app.use(fileUpload())
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended: true}));
+app.use(fileUpload());
 
 /* -------------------------------------------------------------------------- */
 /*                            Configuration MongoDB                           */
 /* -------------------------------------------------------------------------- */
 
-const URL = process.env.URL_MONGODB
+const URL = process.env.URL_MONGODB;
 
-;(async () => {
-  await mongoose.connect(URL).catch(err => {
-    throw new Error(err)
-  })
-})()
+(async () => {
+  await mongoose.connect(URL).catch((err) => {
+    throw new Error(err);
+  });
+})();
 
 /* -------------------------------------------------------------------------- */
 /*                                      /                                     */
@@ -50,7 +49,7 @@ const URL = process.env.URL_MONGODB
 
 app.use(
   session({
-    secret: process.env.SECRET || "mY_seCret",
+    secret: process.env.SECRET || 'mY_seCret',
     store: MongoStore.create({
       mongoUrl: process.env.URL_MONGODB,
       mongoOptions: {
@@ -64,10 +63,10 @@ app.use(
     resave: false,
     saveUninitialized: false,
   })
-)
+);
 
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 /* -------------------------------------------------------------------------- */
 /*                                      /                                     */
@@ -77,38 +76,52 @@ app.use(passport.session())
 /*                                     PUG                                    */
 /* -------------------------------------------------------------------------- */
 
-app.set("views", "./views")
-app.set("view engine", "pug")
+app.set('views', './views');
+app.set('view engine', 'pug');
 
 /* -------------------------------------------------------------------------- */
 /*                                      /                                     */
 /* -------------------------------------------------------------------------- */
 
-const modo = args.MODO || ""
+const modo = args.MODO || '';
 
-if (cluster.isPrimary && modo.toLowerCase() == "cluster") {
-  loggerCons.info({ level: "info" }, `Master ${process.pid} is running`)
+if (cluster.isPrimary && modo.toLowerCase() == 'cluster') {
+  loggerCons.info({level: 'info'}, `Master ${process.pid} is running`);
 
   for (let index = 0; index < cpus; index++) {
-    cluster.fork()
+    cluster.fork();
   }
 
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died`)
-  })
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+  });
 } else {
-  app.use("/", routeLogin)
-  app.use("/register", routeRegister)
-  app.use("/car", routerCar)
-  app.use("/products", routeProducts)
-  app.use("/order", routeOrder)
+  /* -------------------------------------------------------------------------- */
+  /*                                   Routes                                   */
+  /* -------------------------------------------------------------------------- */
 
-  runServer(app)
+  app.use('/', routeLogin);
+  app.use('/register', routeRegister);
+  app.use('/car', routerCar);
+  app.use('/products', routeProducts);
+  app.use('/order', routeOrder);
 
-  app.get("*", async (req, res) => {
-    loggerWarn.warn({ url: `${req.url}` }, "Ruta inexistente")
-    res.json({ message: "No existe la página" })
-  })
+  /* -------------------------------------------------------------------------- */
+  /*                                      /                                     */
+  /* -------------------------------------------------------------------------- */
 
-  loggerCons.info({ level: "info" }, `Worker ${process.pid} started`)
+  const PORT = process.env.PORT || 8080;
+  app.listen(PORT, () => {
+    loggerCons.info(
+      {level: 'info'},
+      `*****RUNNING SERVER ON PORT ${PORT}*****`
+    );
+  });
+
+  app.get('*', async (req, res) => {
+    loggerWarn.warn({url: `${req.url}`}, 'Ruta inexistente');
+    res.json({message: 'No existe la página'});
+  });
+
+  loggerCons.info({level: 'info'}, `Worker ${process.pid} started`);
 }
--- 3_3er Proyecto Final/src/controllers/login.js
+++ 3_3er Proyecto Final/src/controllers/login.js
@@ -1,49 +1,41 @@
-import bcrypt from "bcrypt"
-import { v4 } from "uuid"
-import yargs from "yargs/yargs"
-const args = yargs(process.argv.slice(2)).argv
+import bcrypt from 'bcrypt';
+import yargs from 'yargs/yargs';
+const args = yargs(process.argv.slice(2)).argv;
 
-import { Container } from "../containers/users.js"
-import { sendMail } from "../apis/sendMail.js"
+import {Container} from '../containers/users.js';
+import {sendMail} from '../apis/sendMail.js';
 
-const container = new Container()
+const container = new Container();
 
-export let userLogin = {}
+export let userLogin = {};
 
 export const loginMongodb = {
   authentic: (req, res, next) => {
-    req.isAuthenticated() ? next() : res.redirect("/")
+    req.isAuthenticated() ? next() : res.redirect('/');
   },
 
   passportLogin: async (username, password, done) => {
-    const usersDB = await container.getAll()
+    const usersDB = await container.getAll();
 
-    const user = usersDB.find(userDB => userDB.email === username)
-    if (!user) return done(null, false, { message: "User not exist" })
+    const user = usersDB.find((userDB) => userDB.email === username);
+    if (!user) return done(null, false, {message: 'User not exist'});
 
-    const passIsValide = bcrypt.compareSync(password, user.password)
+    const passIsValide = bcrypt.compareSync(password, user.password);
     if (!passIsValide)
-      return done(null, false, { message: "Password incorrect" })
+      return done(null, false, {message: 'Password incorrect'});
 
-    userLogin = user
+    userLogin = user;
 
-    done(null, user)
+    done(null, user);
   },
 
   passportSignup: async (req, username, password, done) => {
-    const usersDB = await container.getAll()
+    const usersDB = await container.getAll();
 
-    const { name, address, phone, prefijo } = req.body
+    const {name, address, phone, prefijo} = req.body;
 
-    let user = usersDB.find(userDB => userDB.email === username)
-    if (user) return done(null, false, { message: "User already exists" })
-
-    // Sube la imagen al servidor
-    const uuid = v4()
-    let image = req.files.file
-    image.mv(`./public/img/${uuid}-${image.name}`, err => {
-      if (err) return done(null, false, { message: "Error upload file" })
-    })
+    let user = usersDB.find((userDB) => userDB.email === username);
+    if (user) return done(null, false, {message: 'User already exists'});
 
     let newUser = {
       password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null),
@@ -51,14 +43,13 @@ export const loginMongodb = {
       name,
       address,
       phone: `+${prefijo}${phone}`,
-      urlPhoto: `./img/${uuid}-${image.name}`,
-    }
+    };
 
-    userLogin = newUser
+    userLogin = newUser;
 
-    const userMongoDB = await container.save(newUser)
+    const userMongoDB = await container.save(newUser);
 
-    const toEmail = args.EMAIL || "manuele.ramirez.26@gmail.com"
+    const toEmail = args.EMAIL || 'manuele.ramirez.26@gmail.com';
 
     const message = `
     Nombre: ${newUser.name}
@@ -68,16 +59,16 @@ export const loginMongodb = {
     Dirección: ${newUser.address}
     <br>
     Teléfono: ${newUser.phone}
-    `
-    sendMail(toEmail, `Nuevo registro`, message)
+    `;
+    sendMail(toEmail, `Nuevo registro`, message);
 
-    return done(null, userMongoDB)
+    return done(null, userMongoDB);
   },
 
   deserialize: async (id, done) => {
-    const usersDB = await container.getAll()
-    let user = usersDB.find(userDB => userDB.id === id)
+    const usersDB = await container.getAll();
+    let user = usersDB.find((userDB) => userDB.id === id);
 
-    done(null, user)
+    done(null, user);
   },
-}
+};
