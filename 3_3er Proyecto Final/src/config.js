186df01e1a5227e8dade10f53b36b80acc1addea
Manuel Esteban (Merb) <manuele.ramirez.26@gmail.com>
2023-01-09 19:39:10 -0600

    agrego loggerCons con Environment

--- 3_3er Proyecto Final/src/config.js
+++ 3_3er Proyecto Final/src/config.js
@@ -1,14 +1,18 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
+import {loggerCons} from './utils/loggers/logger.js';
 import yargs from 'yargs/yargs';
 const args = yargs(process.argv.slice(2)).argv;
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+loggerCons.info({leve: 'info'}, {environment: NODE_ENV});
 
 export const config = {
   URL_MONGODB: process.env.URL_MONGODB,
   PORT: process.env.PORT || 8080,
   SECRET: process.env.SECRET || 'MY-SECRET',
   MODO: args.MODO || 'fork',
-  NODE_ENV: process.env.NODE_ENV || 'development',
   ADMIN_MAIL: args.MAIL || 'manuele.ramirez.26@gmail.com',
   SESSION_EXPIRES: 600 * 1000,
 };
